ac018cf8df0f2c781df77b81a75503f0eda21182
Kyle Rove <kyle.rove@gmail.com>
2022-08-11 16:27:21 +0000

    Fix table captions with js

--- assets/js/pages/chapter.js
+++ assets/js/pages/chapter.js
@@ -8,6 +8,31 @@ var Chapter = function() {
 	
 	var tableCaptions = function(){
 		$( "p:has(strong.table-number)" ).addClass( "p-table-number" );
+		
+		var tableCaptionWidth = 0;
+		var tableWidth = 0;
+		$(':not(p.p-table-number) + p.p-table-number, * > p.p-table-number:first-of-type').
+		 each(function() {
+		   let tableCaption = $(this);
+		   tableCaptionWidth = $(tableCaption).width();
+		   let table = $(this).nextUntil(':not(table)');
+		   tableWidth = $(table).width();
+		   $(this).
+			   nextUntil(':not(table)').
+			   addBack().
+			   wrapAll('<div class="d-flex table-box" />').
+			   wrapAll('<div class="table-wrapper" />');
+		   // match caption and table width
+		   console.log(tableCaptionWidth);
+		   console.log(tableWidth);
+		   if (tableCaptionWidth > tableWidth) {
+		     tableCaption.width(tableWidth);
+		   }
+		 });
+		
+
+		 
+		/*$('p.p-table-number,table').wrap('<div class="table"></div>');*/
 	};
 	
 	return {
